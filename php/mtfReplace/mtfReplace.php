f8e0cf066f10998b63fc4cfd27955f2ba458b69c
馒头饭 <mhjlw@126.com>
2020-11-15 16:45:09 +0800

    :new:新增mtf替换类

---
+++ php/mtfReplace/mtfReplace.php
@@ -0,0 +1,36 @@
+<?php
+/** mtf替换类 作者：小宇 */
+class mtfReplace {
+    /** 指定位置埋点（区分是否已替换） */
+    private function spacer($ext, $md5) {
+        switch ($ext) {
+            case 'js':
+            case 'css':
+                return "\n" . '/*' . $md5 . '*/' . "\n";
+            break;
+        }
+        return '';
+    }
+    /** 追加（已追加则修改）内容到指定位置 */
+    public function append($rules) {
+        foreach ($rules as $path => $rule) {
+            if (is_file($path)) {
+                $content = file_get_contents($path);
+                $info = pathinfo($path);
+                $ext = $info['extension'];
+                foreach ($rule as $pattern => $replace) {
+                    $md5 = md5($pattern);
+                    $spacer = $this->spacer($ext, $md5);
+                    if (stripos($content, $spacer) === FALSE) {
+                        $content = str_replace($pattern, $pattern . $spacer . $replace . $spacer, $content);
+                    } else {
+                        $spacer = addslashes($spacer);
+                        $content = preg_replace('/(' . $spacer . ').*?(' . $spacer . ')/', '$1' . $replace . '$2', $content);
+                    } 
+                }
+                file_put_contents($path, $content);
+            } 
+        }
+    }
+}
+?>
