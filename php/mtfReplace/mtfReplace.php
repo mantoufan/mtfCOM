9e198777f411458b3f95d8deccdf4b278a52626b
馒头饭 <mhjlw@126.com>
2020-11-16 22:01:48 +0800

    :new:更新替换规则

--- php/mtfReplace/mtfReplace.php
+++ php/mtfReplace/mtfReplace.php
@@ -1,18 +1,36 @@
 <?php
-/** mtf替换类 作者：小宇 */
-class mtfReplace {
+class mtfReplaceUtil {
+    private $ext = '';
+    private $md5 = '';
+    function __construct($ext, $md5) {
+        $this->ext = $ext;
+        $this->md5 = $md5;
+    }
     /** 指定位置埋点（区分是否已替换） */
-    private function spacer($ext, $md5) {
-        switch ($ext) {
+    public function spacer() {
+        switch ($this->ext) {
             case 'js':
             case 'css':
-                return "\n" . '/*yzhan:' . $md5 . '*/' . "\n";
+                return "\n" . '/*yzhan:' . $this->md5 . '*/' . "\n";
             break;
         }
         return '';
     }
+    /** 注释内容 */
+    public function comment($content) {
+        switch ($this->ext) {
+            case 'js':
+            case 'css':
+                return '/*mzhan:' . $this->md5 . $content . '*/';
+            break;
+        }
+        return '';
+    }
+}
+/** mtf替换类 作者：小宇 */
+class mtfReplace {
     /** 追加（已追加则修改）内容到指定位置 */
-    public function append($rules) {
+    public function append($rules, $is_replace = false) {
         foreach ($rules as $path => $rule) {
             if (is_file($path)) {
                 $content = file_get_contents($path);
@@ -20,17 +38,25 @@ class mtfReplace {
                 $ext = $info['extension'];
                 foreach ($rule as $pattern => $replace) {
                     $md5 = md5($pattern);
-                    $spacer = $this->spacer($ext, $md5);
+                    $util = new \mtfReplaceUtil($ext, $md5);
+                    $spacer = $util->spacer();
                     if (stripos($content, $spacer) === FALSE) {
-                        $content = str_replace($pattern, $pattern . $spacer . $replace . $spacer, $content);
+                        $content = str_replace($pattern, ($is_replace ? $util->comment($pattern) : $pattern) . $spacer . $replace . $spacer, $content);
                     } else {
                         $spacer = addcslashes(addcslashes($spacer, '*'), '/');
                         $content = preg_replace('/(' . $spacer . ').*?(' . $spacer . ')/', $replace ? '$1' . $replace . '$2' : '', $content);
+                        if ($is_replace && $replace === '') {
+                            $content = str_replace($util->comment($pattern), $pattern, $content);
+                        }
                     } 
                 }
                 file_put_contents($path, $content);
             } 
         }
+    }
+    /** 替换指定位置的内容，原始内容可复原 */
+    public function replace($rules) {
+        $this->append($rules, true);
     }
 }
 ?>
