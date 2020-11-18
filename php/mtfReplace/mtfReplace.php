e58e1550677a74206fe26d79e6d34e8e41bfff44
馒头饭 <mhjlw@126.com>
2020-11-18 13:44:06 +0800

    :new:适配正则匹配情况

--- php/mtfReplace/mtfReplace.php
+++ php/mtfReplace/mtfReplace.php
@@ -1,34 +1,35 @@
 <?php
-class mtfReplaceUtil {
-    private $ext = '';
-    private $md5 = '';
-    function __construct($ext, $md5) {
-        $this->ext = $ext;
-        $this->md5 = $md5;
-    }
+/** mtf替换类 作者：小宇 */
+class mtfReplace {
     /** 指定位置埋点（区分是否已替换） */
-    public function spacer() {
-        switch ($this->ext) {
+    private function spacer($ext, $md5) {
+        switch ($ext) {
             case 'js':
             case 'css':
-                return "\n" . '/*yzhan:' . $this->md5 . '*/' . "\n";
+            case 'wxss':
+            case 'ttss':
+            case 'qss':
+            case 'acss':
+                return "\n" . '/*yzhan:' . $md5 . '*/' . "\n";
             break;
+
         }
         return '';
     }
     /** 注释内容 */
-    public function comment($content) {
-        switch ($this->ext) {
+    private function comment($ext, $md5, $content) {
+        switch ($ext) {
             case 'js':
             case 'css':
-                return '/*mzhan:' . $this->md5 . $content . '*/';
+            case 'wxss':
+            case 'ttss':
+            case 'qss':
+            case 'acss':
+                return '/*mzhan:' . $md5 . $content . '*/';
             break;
         }
         return '';
     }
-}
-/** mtf替换类 作者：小宇 */
-class mtfReplace {
     /** 追加（已追加则修改）内容到指定位置 */
     public function append($rules, $is_replace = false) {
         foreach ($rules as $path => $rule) {
@@ -38,15 +39,16 @@ class mtfReplace {
                 $ext = $info['extension'];
                 foreach ($rule as $pattern => $replace) {
                     $md5 = md5($pattern);
-                    $util = new \mtfReplaceUtil($ext, $md5);
-                    $spacer = $util->spacer();
-                    if (stripos($content, $spacer) === FALSE) {
-                        $content = str_replace($pattern, ($is_replace ? $util->comment($pattern) : $pattern) . $spacer . $replace . $spacer, $content);
+                    $spacer = $this->spacer($ext, $md5);
+                    if ($spacer === '') {
+                        $content = preg_replace($pattern, $replace, $content);
+                    } else if (stripos($content, $spacer) === FALSE) {
+                        $content = str_replace($pattern, ($is_replace ? $this->comment($ext, $md5, $pattern) : $pattern) . $spacer . $replace . $spacer, $content);
                     } else {
                         $spacer = addcslashes(addcslashes($spacer, '*'), '/');
                         $content = preg_replace('/(' . $spacer . ').*?(' . $spacer . ')/', $replace ? '$1' . $replace . '$2' : '', $content);
                         if ($is_replace && $replace === '') {
-                            $content = str_replace($util->comment($pattern), $pattern, $content);
+                            $content = str_replace($this->comment($ext, $md5, $pattern), $pattern, $content);
                         }
                     } 
                 }
