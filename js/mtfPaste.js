5001fe898a5c14f1af3d2b193b044d83f304bab5
馒头饭 <mhjlw@126.com>
2022-07-31 02:11:54 +0800

    :new:更新粘贴，不再兼容 IE

--- js/mtfPaste.js
+++ js/mtfPaste.js
@@ -25,9 +25,9 @@ var mtfPaste={
 	},
 	run:function(_b,_f){
 		// 干掉IE http之类地址自动加链接
-		try {
-			document.execCommand("AutoUrlDetect", false, false);
-		} catch (e) {}
+		// try {
+		// 	document.execCommand("AutoUrlDetect", false, false);
+		// } catch (e) {}
 		
 		var e=event,s=window.getSelection ? window.getSelection() : window.document.selection,r=s.rangeCount > 0 ? s.getRangeAt(0) : window.document.createRange(),n=document.createElement('DIV'),c=document.createTextNode('_'),_s,_s0;
 		n.appendChild(c);
@@ -38,14 +38,14 @@ var mtfPaste={
 		s.removeAllRanges();
 		s.addRange(r);
 
-		if(window.clipboardData && clipboardData.setData) {
+		// if(window.clipboardData && clipboardData.setData) {
 			// IE
-			_s = window.clipboardData.getData('text');
-			_h = '';
-		} else {
+		// 	_s = window.clipboardData.getData('text');
+		// 	_h = '';
+		// } else {
 			_s = (e.originalEvent || e).clipboardData.getData('text/plain');
 			_h = (e.originalEvent || e).clipboardData.getData('text/html');
-		}
+		// }
 		_s=mtfPaste.filter(_s, _h);
 		if(_b){
 			_s=_b(_s);
@@ -54,35 +54,35 @@ var mtfPaste={
 			if(!mtfPaste.cancel){
 				if(n.parentNode){
 					n.parentNode.removeChild(n);
-					if (document.body.createTextRange) {    
-						if (document.selection) {
-							textRange = document.selection.createRange();
-						} else if (window.getSelection) {
-							sel = window.getSelection();
-							var range = sel.getRangeAt(0);
+					// if (document.body.createTextRange) {    
+					// 	if (document.selection) {
+					// 		textRange = document.selection.createRange();
+					// 	} else if (window.getSelection) {
+					// 		sel = window.getSelection();
+					// 		var range = sel.getRangeAt(0);
 
-							// 创建临时元素，使得TextRange可以移动到正确的位置
-							var tempEl = document.createElement("span");
-							tempEl.innerHTML = "&#FEFF;";
-							range.deleteContents();
-							range.insertNode(tempEl);
-							textRange = document.body.createTextRange();
-							textRange.moveToElementText(tempEl);
-							tempEl.parentNode.removeChild(tempEl);
-						}
-						textRange.text = _s;
-						textRange.collapse(false);
-						textRange.select();
-					} else {
+					// 		// 创建临时元素，使得TextRange可以移动到正确的位置
+					// 		var tempEl = document.createElement("span");
+					// 		tempEl.innerHTML = "&#FEFF;";
+					// 		range.deleteContents();
+					// 		range.insertNode(tempEl);
+					// 		textRange = document.body.createTextRange();
+					// 		textRange.moveToElementText(tempEl);
+					// 		tempEl.parentNode.removeChild(tempEl);
+					// 	}
+					// 	textRange.text = _s;
+					// 	textRange.collapse(false);
+					// 	textRange.select();
+					// } else {
 						// Chrome之类浏览器
 						document.execCommand("insertText", false, _s);
-					}
+					// }
 				}
 			}
 			if(_f){
 				_f();
 			}
-		},100);
+		},4);
 		
 		/*
 		var s = null,e=event;
