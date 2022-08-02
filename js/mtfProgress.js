81f82a5aba4fc8bf3e5ab2c630bfa56437bf024a
馒头饭 <mhjlw@126.com>
2022-08-02 19:04:22 +0800

    :new:精简请求进度滚动条

--- js/mtfProgress.js
+++ js/mtfProgress.js
@@ -1,26 +1,22 @@
 var mtfProgress={
-	off:0,
-	start:function(){
-		if(this.off){
+	off: 0,
+	start: function(){
+		if (this.off) {
 			this.off=0;
 			return false;
 		}
-		var d=$('<div>');
-		d.addClass('mtf-progress');
-		d.css({'width':'0%','position':'fixed','height':'.3em','top':'0','background':'black','opacity':'.5','z-index':'7'});
-		$('body').append(d);
-		d.animate({'width':'90%'},{
-			duration:1000,
-			complete:function(){
-				$(this).remove();
-			}
-		 })
+		$('<div>').addClass('mtf-progress').css({'width':'0%','position':'fixed','height':'2px','top':0,'background':'#000','opacity':.2,'z-index':7})
+		.appendTo($('body')).animate({'width':'90%'}, {
+			duration: 1500,
+			complete: this.complete
+		})
 	},
-	end:function(){
-		$('.mtf-progress').animate({'width':'100%','opacity':'0'},{
-			complete:function(){
-				$(this).remove();
-			}
-		 });
+	end: function(){
+		$('.mtf-progress').stop().animate({'width':'100%', 'opacity': 0}, {
+			complete: this.complete
+		})
+	},
+	complete: function() {
+		$(this).remove()
 	}
 };
