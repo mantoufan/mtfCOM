195b4a257fb9143e873f1cd052eca362dc433d51
馒头饭 <mhjlw@126.com>
2022-09-03 00:46:29 +0800

    :wastbasket:废弃 p2p 网址的 if = 1 识别参数:zap:精简 modSSO 代码

--- mod/mod+/modSSO/js.js
+++ mod/mod+/modSSO/js.js
@@ -1,18 +1,12 @@
-MTF.Var.login.i=function(){
-	var a=window.location.href.split('?');a.splice(0,1);a.join('?');a=MTF.Render.parseStr(a.join('?'));
-	if(a.r){
-		var f=function(i,u){
-			top.location.href=decodeURIComponent(a.r.split('#')[0])+'#i='+i+'&uid='+u;
-		};
-		MTF.Var.login.t=mtfLang.get(['请','登录']);
-		$('.qq-login').click();
-		MTF.Var.login.f=function(i,u){
-			f(i,u);
-		};
-		$("<style></style>").text(".ply-overlay{background:url(https://api.os120.com/img/bg/?out_type=redirect&des=mtfimssologin&order=rand&theme=fanxiaoxi) no-repeat center;background-size:cover}.ply-layer{opacity:.75}.ply-name{float:left;padding:0 10px;margin:-10px 10px 0 -20px;line-height:44px;font-size:14px;color:white;background-color:#fc89a1;}.ply-back{float:right}").appendTo($("head"));
-		$('.ply-overlay').attr('data-ply','');
-		$('.ply-header').append($('.ply-cancel').clone().addClass('ply-back').html(mtfLang.get(['返回'])).attr('data-ply','').click(function(){history.back();}))
-		.prepend($('<div>').html(mtfLang.get(['M站','通行证'])).addClass('ply-name'));
+(function(){
+	$('body>div').hide()
+	var a = MTF.Render.parseStr(window.location.search)
+	if(a.r === void 0) return
+	MTF.Var.login.t = mtfLang.get(['请','登录'])
+	$('.qq-login').trigger('click')
+	MTF.Var.login.f = function(i, u) {
+		top.location.href = decodeURIComponent(a.r.split('#')[0]) + '#i=' + i + '&uid=' + u
 	}
-};
-MTF.Var.login.i();
+	$("<style></style>").text(".ply-overlay{background:url(https://api.os120.com/img/bg/?out_type=redirect&des=mtfimssologin&order=rand&theme=fanxiaoxi) no-repeat center;background-size:cover}.ply-layer{opacity:.75}.ply-name{float:left;padding:0 10px;margin:-10px 10px 0 -20px;line-height:44px;font-size:14px;color:white;background-color:#fc89a1;}.ply-back{float:right}").appendTo($("head"))
+	$('.ply-header').append($('.ply-cancel').clone().addClass('ply-back').html(mtfLang.get(['返回'])).on('click', function(){history.back()})).prepend($('<div>').html(mtfLang.get(['M站','通行证'])).addClass('ply-name'))
+})()
