bcf8d2e0803199ab8b1f6c4a7fbac5aff18958ce
馒头饭 <mhjlw@126.com>
2023-09-30 16:12:25 +0800

    :wrench:mv os120.com to yzhan.cyou

--- mod/mod+/modSSO/js.js
+++ mod/mod+/modSSO/js.js
@@ -7,6 +7,6 @@
 	MTF.Var.login.f = function(i, u) {
 		top.location.href = decodeURIComponent(a.r.split('#')[0]) + '#i=' + i + '&uid=' + u
 	}
-	$("<style></style>").text(".ply-overlay{background:url(https://api.os120.com/img/bg/?out_type=redirect&des=mtfimssologin&order=rand&theme=fanxiaoxi) no-repeat center;background-size:cover}.ply-layer{opacity:.75}.ply-name{float:left;padding:0 10px;margin:-10px 10px 0 -20px;line-height:44px;font-size:14px;color:white;background-color:#fc89a1;}.ply-back{float:right}").appendTo($("head"))
+	$("<style></style>").text(".ply-overlay{background:url(https://api.yzhan.cyou/img/bg/?out_type=redirect&des=mtfimssologin&order=rand&theme=fanxiaoxi) no-repeat center;background-size:cover}.ply-layer{opacity:.75}.ply-name{float:left;padding:0 10px;margin:-10px 10px 0 -20px;line-height:44px;font-size:14px;color:white;background-color:#fc89a1;}.ply-back{float:right}").appendTo($("head"))
 	$('.ply-header').append($('.ply-cancel').clone().addClass('ply-back').html(mtfLang.get(['返回'])).on('click', function(){history.back()})).prepend($('<div>').html(mtfLang.get(['M站','通行证'])).addClass('ply-name'))
 })()
