dc6a889c67164032dc8f58ef1d70c814004c4ab8
馒头饭 <mhjlw@126.com>
2023-09-30 15:56:13 +0800

    :rocket:add support to xiaomark.com

--- php/mtfTcn/mtfTcn.php
+++ php/mtfTcn/mtfTcn.php
@@ -7,18 +7,16 @@ class mtfTcn{
 		define('SINA_APPKEY', $this->appkey);
 		define('w3_APPKEY', $this->w3key);
 	}
-	private function _curl($url)
+	private function _curl($url, $header = null, $post = null)
     {
-		//设置附加HTTP头  
-		$addHead = array(  
-			"Content-type: application/json"  
-		);  
 		//初始化curl，当然，你也可以用fsockopen代替  
 		$ch = curl_init();  
 		//设置网址  
 		curl_setopt($ch, CURLOPT_URL, $url);  
 		//附加Head内容  
-		//curl_setopt($ch, CURLOPT_HTTPHEADER, $addHead);  
+		if ($header !== null) {
+		  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		}  
 		//是否输出返回头信息  
 		curl_setopt($ch, CURLOPT_HEADER, 0);  
 		//将curl_exec的结果返回  
@@ -31,6 +29,10 @@ class mtfTcn{
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 		curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
 		curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 600);//DNS缓存时间，改为10分钟
+
+		if ($post !== null) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		}
 		
 		//执行  
 		$result = curl_exec($ch);  
@@ -57,29 +59,17 @@ class mtfTcn{
 		}	
 	}  
 	//根据长网址获取短网址  
-	public function short($long_url) {  
-		$long_url=$this->_filterUrl($long_url);
-		if ($long_url === FALSE) {
-			return FALSE;
-		}
-		//拼接请求地址，此地址你可以在官方的文档中查看到  
-		//$url = 'http://api.t.sina.com.cn/short_url/shorten.json?source=' . SINA_APPKEY .'&url_long=' . $long_url;  
-		//$url = 'http://api.3w.cn/api.php?key=' . w3_APPKEY .'&url=' . $long_url; 
-		//$url = 'http://a.os120.com/surl/?url=' . $long_url; 
-		$url = 'http://api.suowo.cn/api.htm?key=5e948905b1b63c650a02575a@4cbf344b3bff3c207dfe460004a43cf1&domain=2&expireDate='.date('Y-m-d', strtotime('+10year')).'&url='.$long_url;
-		//$url = 'http://api.dm126.com/plugin/Suowo/?url=' . $long_url;
-		//获取请求结果  
-		$result = $this->_curl($url);
-		//下面这行注释用于调试，你可以把注释去掉看看从sina返回的信息是什么东西  
-		//print_r($result);exit();  
-		//解析json  
-//		$json = json_decode($result);  
-//		异常情况返回false  
-//		if (isset($json->error) || !isset($json[0]->url_short) || $json[0]->url_short =='')  
-//			return false;  
-//		else  
-//			return $json[0]->url_short;  
-		return $result ? $result : FALSE;
+	public function short($long_url) {
+		$url = 'https://api.xiaomark.com/v1/link/create'; // 小马短链接 
+		$result = $this->_curl($url, array(  
+			'content-type: application/json'
+		), json_encode(array(
+			'apikey' => '689a5b4bfa3a492e1874c0ad5e171b75', // 自己去申请 key ，每天 50 条，不用用我的 ^_^
+			'origin_url' => $long_url
+		)));
+    $json = json_decode($result);
+		if ($json->code !== 0) return FALSE;
+		return $json->data->link->url;
 	}  
 	//根据短网址获取长网址，此函数重用了不少sinaShortenUrl中的代码，以方便你阅读对比，你可以自行合并两个函数  
 	public function expand($short_url) {  
